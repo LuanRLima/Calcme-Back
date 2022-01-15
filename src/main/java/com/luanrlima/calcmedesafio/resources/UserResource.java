0ee273f91c3ad9e55d66f2f0ac058d0729565793
LuanRLima <luanrlima9@gmail.com>
2022-01-15 18:58:14 -0300

    Obetendo um usuario por id

--- src/main/java/com/luanrlima/calcmedesafio/resources/UserResource.java
+++ src/main/java/com/luanrlima/calcmedesafio/resources/UserResource.java
@@ -3,11 +3,13 @@ package com.luanrlima.calcmedesafio.resources;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +31,10 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDTO);
 		
 	}
-
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
 }
---
+++ src/main/java/com/luanrlima/calcmedesafio/services/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.luanrlima.calcmedesafio.services;
+
+public class ObjectNotFoundException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+		
+}
