03d70b7671ab17813cdbc8a23718eaee2e20b315
LuanRLima <luanrlima9@gmail.com>
2022-01-15 16:28:37 -0300

    Primeiro commit commit - projeto criado

---
+++ src/main/java/com/luanrlima/calcmedesafio/CalcmedesafioApplication.java
@@ -0,0 +1,13 @@
+package com.luanrlima.calcmedesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcmedesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcmedesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luanrlima/calcmedesafio/CalcmedesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luanrlima.calcmedesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcmedesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
